af385ce09c2d98fabf94002b8c10f14bd1947ec9
José Padilla <jpadilla@webapplicate.com>
2015-03-25 13:27:47 -0400

    Use default reason phrases from HTTP standard.
    Closes #2763
    Conflicts:
    	rest_framework/response.py

--- rest_framework/response.py
+++ rest_framework/response.py
@@ -6,6 +6,7 @@ The appropriate renderer is called during Django's template response rendering.
 """
 from __future__ import unicode_literals
 import django
+from django.utils.six.moves.http_client import responses
 from django.template.response import SimpleTemplateResponse
 from django.utils import six
 from rest_framework.compat import responses
